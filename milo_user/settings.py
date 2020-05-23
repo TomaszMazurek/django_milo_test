77096c37bfabd4060367cca3c6e3e8d11b0ea022
tomasz.a.mazurek@gmail.com <Tomasz.A.Mazurek@gmail.com>
2020-05-23 16:12:55 +0200

    custom user and manager

--- milo_user/settings.py
+++ milo_user/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'user.MiloUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
